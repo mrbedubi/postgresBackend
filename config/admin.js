2df50416d72db747c872442c62bfa8f76d55339b
Mr.Bedubi <diogo.azev97@gmail.com>
2023-03-05 04:45:11 +0000

    seccondpush

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  url: '/dashboard',
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,9 +3,7 @@ module.exports = ({ env }) => ({
   proxy: true,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('APP_URL','dev-mudamuda.dpixel.pt'), // Sets the public URL of the application.
   app: {
-    url: env('APP_URL','dev-mudamuda.dpixel.pt/admin'), // Sets the public URL of the application.
     keys: env.array('APP_KEYS')
   },
 });
