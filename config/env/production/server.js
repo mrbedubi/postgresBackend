9f5e2c5d4630415a51ce755ea198b6281def72c9
Mr.Bedubi <diogo.azev97@gmail.com>
2023-03-05 03:12:48 +0000

    s

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,9 @@
 
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('APP_URL'), // Sets the public URL of the application.
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('APP_URL','dev-mudamuda.dpixel.pt'), // Sets the public URL of the application.
   app: {
     keys: env.array('APP_KEYS')
   },
