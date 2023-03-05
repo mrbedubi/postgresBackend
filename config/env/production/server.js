6d79865fc3f18eec6eb7066074716e079f6ed402
Mr.Bedubi <diogo.azev97@gmail.com>
2023-03-05 04:21:25 +0000

    seccondpush

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('APP_URL','dev-mudamuda.dpixel.pt'), // Sets the public URL of the application.
   app: {
+    url: env('APP_URL','dev-mudamuda.dpixel.pt/admin'), // Sets the public URL of the application.
     keys: env.array('APP_KEYS')
   },
 });
