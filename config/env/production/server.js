40861f2bb116d28e3a201e11e61b3e8b6529f4fa
Mr.Bedubi <diogo.azev97@gmail.com>
2023-03-05 12:25:24 +0000

    seccondpush

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,15 @@
 
 module.exports = ({ env }) => ({
-  proxy: true,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
+  },
+  socket: '/tmp/nginx.socket', // only use if absolutely required
+  emitErrors: false,
+  url: env('PUBLIC_URL', 'https://api.example.com'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
   },
 });
