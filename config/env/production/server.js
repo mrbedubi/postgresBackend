63572a24530e69d4c3c4c234d53a4ed8e8ea1c99
Mr.Bedubi <diogo.azev97@gmail.com>
2023-03-05 13:00:32 +0000

    seccondpush

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,13 @@
 
 module.exports = ({ env }) => ({
-  proxy: true,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
+  },
+  emitErrors: true,
+  proxy: true,
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
   },
 });
