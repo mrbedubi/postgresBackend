987f3b4356f20b3f0b1f2327e7e35ac2475a3c48
Mr.Bedubi <diogo.azev97@gmail.com>
2023-03-05 12:43:02 +0000

    seccondpush

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,14 @@
 
 module.exports = ({ env }) => ({
-  proxy: true,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
+  },
+  emitErrors: true,
+  url: env('PUBLIC_URL', 'https://dev-mudamuda.dpixel.pt'),
+  proxy: true,
+  cron: {
+    enabled: env.bool('CRON_ENABLED', true),
   },
 });
