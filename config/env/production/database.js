46e6b42c52beb96b2b09d6085cc4e2eaa4e16e7a
Mr.Bedubi <diogo.azev97@gmail.com>
2023-04-28 22:09:58 +0100

    first_deploy

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,13 @@
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-      },
-    },
-    debug: false,
-  },
+module.exports =  ({ env }) => ({
+	connection: {
+		client: 'postgres',
+		connection: {
+		host: env('DATABASE_HOST', 'localhost'),
+			port: env.int('DATABASE_PORT', 5432),
+			database: env('DATABASE_NAME', 'dbstrapi'),
+			user: env('DATABASE_USERNAME', 'postgres'),
+			password: env('DATABASE_PASSWORD', 'admin'),
+			ssl: env.bool('DATABASE_SSL', false)
+		}
+	}
 });
